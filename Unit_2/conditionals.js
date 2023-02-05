0279ee5a4e20a6cbca3a59bd51e9124ecea272a5
Raven Fisher <ravenfisher1122@gmail.com>
2023-02-05 15:39:40 -0700

    Conditionals

--- Unit_2/conditionals.js
+++ Unit_2/conditionals.js
@@ -180,3 +180,44 @@ switch (true) {
     default:
         console.log("That's not a grade value...")
 }
+
+// ? Ternary 
+/*
+    Always a If/Else style conditional at minimum.
+    Structure:
+    expression ? true : false;
+*/
+
+let expression = false;
+expression ? console.log("Runs true") : console.log("Runs false");
+
+// as an if/else
+if (newNum > 0) {
+    console.log("yes");
+} else {
+    console.log("no");
+}
+
+    // ? Else/If
+
+let anotherNum = 3
+
+if (anotherNum == 0) {
+    console.log("hello");
+} else if (anotherNum > 0) {
+    console.log("Hey There!");
+} else {
+    console.log("Goodbye");
+}
+
+anotherNum == 0
+  ? console.log("Hello")
+  : anotherNum > 0
+  ? console.log("hi")
+  : console.log("goodbye again");
+
+let numOne = -5;
+let numTwo = 2;
+
+let value = numOne + numTwo > 1 ? numOne + numTwo : 0;
+console.log(value);
